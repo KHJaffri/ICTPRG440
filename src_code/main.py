21b7bcbec27ca390e8a20e612a2b76b7bf1cd87f
KHJaffri <Syed.Jaffri@studytafensw.edu.au>
2025-11-23 19:33:38 +1100

    Update main.py

--- src_code/main.py
+++ src_code/main.py
@@ -2,3 +2,10 @@ def add2numbers(x, y):
     return x+y
 print("KAshanHyderJaffri")
 print("TAFENSW")
+print( "KashanHyderJaffri")
+print("TAFENSW")
+print("Task1completed")
+def myfunction (x,y,z):
+    s=x+y+z
+    return s
+d = myfunction (1,2,3)
